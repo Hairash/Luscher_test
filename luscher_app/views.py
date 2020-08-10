b842768f0d90d3927786e8fd27b4e0ed117d16c4
Hairash <maslevian@yandex.ru>
2020-08-11 01:48:20 +0300

    Add game logics
    * No shuffle yet
    * No finish label

--- luscher_app/views.py
+++ luscher_app/views.py
@@ -16,7 +16,9 @@ def index(request):
     # Пример:
     # 07612453
     # 67543120
-    colors = [
+    return render(request, 'index.html', {'colors': colors})
+colors = [
+    [
         {'num': 1, 'color': '#1d36b4'},
         {'num': 2, 'color': '#339e8a'},
         {'num': 3, 'color': '#b06121'},
@@ -25,5 +27,15 @@ def index(request):
         {'num': 6, 'color': '#ba5921'},
         {'num': 7, 'color': '#000000'},
         {'num': 0, 'color': '#8a8a8a'},
-    ]
-    return render(request, 'index.html', {'colors': colors})
+    ],
+    [
+        {'num': 2, 'color': '#339e8a'},
+        {'num': 6, 'color': '#ba5921'},
+        {'num': 4, 'color': '#ffef42'},
+        {'num': 0, 'color': '#8a8a8a'},
+        {'num': 7, 'color': '#000000'},
+        {'num': 5, 'color': '#7857ff'},
+        {'num': 3, 'color': '#b06121'},
+        {'num': 1, 'color': '#1d36b4'},
+    ],
+]
---
+++ templates/script.js
@@ -0,0 +1,30 @@
+NUM_COLORS = 8;
+NUM_ROUNDS = 2;
+
+window.onload = () => {
+    round = 1;
+    $('.block').click( function() {
+        console.log($(this).attr('num'));
+        $(this).addClass('hide');
+        $('#result')[0].value += $(this).attr('num');
+        if ($(`.color_set[num=${round}] .block.hide`).length == NUM_COLORS) {
+            change();
+        }
+    } );
+}
+
+function change() {
+    if (round >= NUM_ROUNDS) {
+        end();
+    }
+    else {
+        $(`.color_set[num=${round}]`).addClass('hide');
+        round += 1;
+        $(`.color_set[num=${round}]`).removeClass('hide');
+        $('#result')[0].value += '\r\n';
+    }
+}
+
+function end() {
+    $('#result').removeClass('hide');
+}
